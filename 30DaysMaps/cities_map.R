6da4d55c25d32699cb788f1736e33d3745407cf0
Thom-Ivar van Dijk <ti@Thom-Ivar.local>
2021-11-13 12:03:27 +0100

    adjusted cities_map

--- 30DaysMaps/cities_map.R
+++ 30DaysMaps/cities_map.R
@@ -18,17 +18,17 @@ df <- readOGR( dsn = tempUnzipped, verbose = FALSE) %>%  #read shapefile
 
 ggplot(df) +
   geom_point( aes(x = lon, y = lat), 
-              alpha = 0.6, color = 'grey20', size = 0.25) + 
+              alpha = 0.7, color = '#9C918E', size = 0.25) + 
   labs(title = 'Cities with more than 100,000 inhabitants', 
        subtitle = '(source: Natural Earth)',
        caption = '@ThomIvar') +
   theme_void() +
   theme(plot.background = element_rect(fill = '#fffef7', color = '#fffef7'), 
-        text = element_text('American Typewriter', color = 'grey30'),
-        plot.title = element_text(hjust = 0.5, size = 24, face = 'bold'), 
+        text = element_text('American Typewriter', color = '#9C918E'),
+        plot.title = element_text(hjust = 0.5, size = 26, face = 'bold'), 
         plot.subtitle = element_text(hjust = 0.5, size = 10), 
-        plot.caption = element_text(size = 10, color = 'grey70'), 
+        plot.caption = element_text(size = 8, color = 'grey80'), 
         plot.margin = margin(0.5, 0.5, 0.5, 0.5, unit = 'cm'))
 
-ggsave("Nature_Earth/cities_map.png", width = 10, height = 6)
+ggsave("30DaysMaps/cities_map.png", width = 10, height = 6)
 
